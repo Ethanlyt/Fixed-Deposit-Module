d232fc050e30b5858de3604df55e6f062b5d02e4
Ethan0l <44204733+Ethanlyt@users.noreply.github.com>
2022-08-24 16:32:11 +0800

    Delete BackEnd directory

--- BackEnd/src/main/java/com/leong/pelaburan/Repository/RegistrationRepo.java
+++
@@ -1,7 +0,0 @@
-package com.leong.pelaburan.Repository;
-
-import com.leong.pelaburan.entity.Registration;
-
-public interface RegistrationRepo extends CustomizedRepo<Registration, Long> {
-
-}
--- BackEnd/src/main/java/com/leong/pelaburan/Repository/WithdrawRepo.java
+++
@@ -1,7 +0,0 @@
-package com.leong.pelaburan.Repository;
-
-import com.leong.pelaburan.entity.Withdraw;
-
-public interface WithdrawRepo extends CustomizedRepo<Withdraw, Long> {
-
-}
