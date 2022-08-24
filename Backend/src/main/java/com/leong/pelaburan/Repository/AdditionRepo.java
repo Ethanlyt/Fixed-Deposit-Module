425cd11716ee9f1f19a8fa6fa5360dcf2d830b91
Ethan0l <44204733+Ethanlyt@users.noreply.github.com>
2022-08-24 20:13:57 +0800

    Delete Backend directory

--- Backend/src/main/java/com/leong/pelaburan/Repository/AdditionRepo.java
+++
@@ -1,7 +0,0 @@
-package com.leong.pelaburan.Repository;
-
-import com.leong.pelaburan.entity.Addition;
-
-public interface AdditionRepo extends CustomizedRepo<Addition, Long> {
-
-}
--- Backend/src/main/java/com/leong/pelaburan/controller/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.leong.pelaburan.controller;
-
-import com.leong.pelaburan.entity.LoginDto;
-
-public interface UserService {
-    public ApiResponse login(LoginDto loginDto);
-    public boolean validateUsername(String username);
-}
